15d012e6e1f2a9b7f55786ba393551d1b4979954
Emin Özkaradeniz <emin.ozkaradeniz@gmail.com>
2023-01-22 17:45:15 +0300

    Update HomeController.java

--- app/src/main/java/com/hotelManagementApp/app/controller/HomeController.java
+++ app/src/main/java/com/hotelManagementApp/app/controller/HomeController.java
@@ -1,14 +1,31 @@
 package com.hotelManagementApp.app.controller;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String showHome() {
+    public String showHome(Authentication authentication) {
+
+        // check if any user logged in
+        Collection<? extends GrantedAuthority> authorities = null;
+
+        try {
+            authorities = authentication.getAuthorities();
+        } catch (NullPointerException e) {
+            System.out.println("No user logged in");
+            return "login-page";
+        }
+
+        System.out.println(authentication.getName() + " logged in.");
+
         return "home";
     }
 }
