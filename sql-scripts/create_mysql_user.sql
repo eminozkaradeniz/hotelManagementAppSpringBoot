996d2e4fc4d301e8ba858d5bf873a35ee30f9ccd
Emin Özkaradeniz <emin.ozkaradeniz@gmail.com>
2023-09-05 15:28:15 +0300

    Update create_mysql_user.sql

--- sql-scripts/create_mysql_user.sql
+++ sql-scripts/create_mysql_user.sql
@@ -1,3 +1,2 @@
-CREATE USER 'hotel_management_app'@'localhost' 
-IDENTIFIED WITH mysql_native_password BY 'hotel_management_app';
+CREATE USER 'hotel_management_app'@'localhost' IDENTIFIED BY 'hotel_management_app';
 GRANT ALL PRIVILEGES ON * . * TO 'hotel_management_app'@'localhost';
